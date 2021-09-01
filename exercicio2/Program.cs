5d097cb9b2788c745fe7de7e62155382a252546a
Thomas Redekop <redetho@gmail.com>
2021-09-01 20:13:32 -0300

    adição de comentários

--- exercicio2/Program.cs
+++ exercicio2/Program.cs
@@ -9,14 +9,18 @@ namespace exercicio2
 
         static void Main(string[] args)
         {
+            //declarando variáveis
                 string nome1;
                 string nome2;
             int idade1;
             int idade2;
+            //pede os nomes e idades
             Console.WriteLine("digite o primeiro nome");
             nome1 = Console.ReadLine();
+            //limpa o texto para não ficar poluído
             Console.Clear();
             Console.WriteLine("digite a primeira idade");
+            //converte a idade string para int
             idade1 = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
             Console.WriteLine("digite o segundo nome");
@@ -25,6 +29,7 @@ namespace exercicio2
             Console.WriteLine("digite a segunda idade");
             idade2 = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
+            //mostra qual idade é maior e declara seu nome
             if(idade1 < idade2)
             {
                 Console.WriteLine("Pessoa mais velha:" + nome2);
