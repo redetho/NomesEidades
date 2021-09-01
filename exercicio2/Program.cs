c3f28db3c701c5032c6babb2afae8659e87d06e6
Thomas Redekop <redetho@gmail.com>
2021-09-01 18:11:06 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace exercicio2
+{
+
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+                string nome1;
+                string nome2;
+            int idade1;
+            int idade2;
+            Console.WriteLine("digite o primeiro nome");
+            nome1 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("digite a primeira idade");
+            idade1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("digite o segundo nome");
+            nome2 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("digite a segunda idade");
+            idade2 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            if(idade1 < idade2)
+            {
+                Console.WriteLine("Pessoa mais velha:" + nome2);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha:" + nome1);
+            }
+
+        }
+    }
+}
